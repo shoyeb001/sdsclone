9eb324d4e8067407917b7c08c563368239b5d258
Sk Shoyeb <52367870+shoyeb001@users.noreply.github.com>
2021-12-09 12:07:00 +0530

    sidebar update

--- resources/views/Fontend/products.blade.php
+++ resources/views/Fontend/products.blade.php
@@ -84,10 +84,47 @@
                                 <i class="fab fa-whatsapp-square"></i><span>Inquiry</span>
                             </a>
                         </div>
-                        <div><i class="fas fa-phone-square-alt"></i><span>Call</span></div>
+                        <div><a href="tel:+91%209892080847">
+                            <i class="fas fa-phone-square-alt"></i><span>Call</span>
+                            </a></div>
                     </div>
                 </div>
             @endforeach
         </div>
     </section>
 @endsection
+<script>
+    let showme = document.getElementById("ShowMe");
+    let addClassNow = document.querySelector("#mNavSec");
+    const fil = document.querySelector("#closebutton");
+
+    let count = true;
+    showme.addEventListener('click', () => {
+        addClassNow.classList.toggle("classHide");
+        val = addClassNow.classList.contains('classHide');
+        // if (val) {
+        //     $(document).ready(function () {
+        //         console.log("This is added now");
+        //         $(document).on('click', function (divclose) {
+        //             if ($(divclose.target).closest("#filterid").length == 0) {
+        //                 $("#filterid").hide();
+        //             }
+        //         })
+        //     })
+        // }
+    })
+    fil.addEventListener('click', () => {
+        addClassNow.classList.remove("classHide");
+    })
+
+
+
+        // $(document).ready(function () {
+        //     console.log("This is added now");
+        //     $(document).on('click', function (divclose) {
+        //         if ($(divclose.target).closest("#filterid").length == 0) {
+        //             $("#filterid").hide();
+        //         }
+        //     })
+        // })
+</script>
